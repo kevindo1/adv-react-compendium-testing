27286119391ae63c75ef1a7894f06b19daa05a84
Kevin Do <kevind0501@gmail.com>
2022-01-25 17:28:48 -0800

    new nodejs

--- src/views/Home.test.js
+++ src/views/Home.test.js
@@ -34,10 +34,10 @@ test('Should be able to search Pokemon', async () => {
   // the results that user gets back
   const pokemon = await screen.findAllByText(pokemonName, { exact: false });
 
-  // const result = pokemon.map((item) => item.textContent);
+  const result = pokemon.map((item) => item.textContent);
 
-  // const handleNameCheck = (name) => name.toLowerCase().includes(pokemonName);
+  const handleNameCheck = (name) => name.toLowerCase().includes(pokemonName);
 
-  // const hasSameName = result.every(handleNameCheck);
-  // expect(hasSameName).toBe(true);
+  const hasSameName = result.every(handleNameCheck);
+  expect(hasSameName).toBe(true);
 });
