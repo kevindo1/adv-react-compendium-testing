cafc4d84839021d86145f6bb44c1a8b153aede74
Kevin Do <kevind0501@gmail.com>
2022-01-27 16:23:47 -0800

    passing MSW test

--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -1,11 +1,11 @@
-// export async function getPokemon(query) {
-//   const params = new URLSearchParams();
-//   params.set('pokemon', query);
+export async function getPokemon(query) {
+  const params = new URLSearchParams();
+  params.set('pokemon', query);
 
-//   const response = await fetch(
-//     `https://pokedex-alchemy.herokuapp.com/api/pokedex?${params.toString()}`
-//   );
-//   const data = await response.json();
+  const response = await fetch(
+    `https://pokedex-alchemy.herokuapp.com/api/pokedex?${params.toString()}`
+  );
+  const data = await response.json();
 
-//   return data;
-// }
+  return data;
+}
